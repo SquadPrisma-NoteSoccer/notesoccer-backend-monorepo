fbe2c033c6e793d5f6fb6d960136a43a1ffb8d97
Leonardo A. C. Ferraz <leoalvo89@gmail.com>
2025-10-06 14:35:25 -0300

    Adiciona anotações Swagger no OrchestrationUsuarioController

--- orchestration-service/src/main/java/com/squadprisma/notesoccer/orchestration_service/api/controller/OrchestrationUsuarioController.java
+++ orchestration-service/src/main/java/com/squadprisma/notesoccer/orchestration_service/api/controller/OrchestrationUsuarioController.java
@@ -3,6 +3,8 @@ package com.squadprisma.notesoccer.orchestration_service.api.controller;
 import com.squadprisma.notesoccer.orchestration_service.api.dto.CreateUsuarioRequest;
 import com.squadprisma.notesoccer.orchestration_service.api.dto.UsuarioResponse;
 import com.squadprisma.notesoccer.orchestration_service.application.service.UserOrchestrationService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Usuarios")
 @RestController
 @RequestMapping("/orchestration/v1/usuarios")
 public class OrchestrationUsuarioController {
@@ -21,6 +24,7 @@ public class OrchestrationUsuarioController {
         this.service = service;
     }
 
+    @Operation(summary = "Cadastra um usuário")
     @PostMapping
     public ResponseEntity<UsuarioResponse> criar(@Valid @RequestBody CreateUsuarioRequest req) {
         var created = service.criarUsuario(req);
