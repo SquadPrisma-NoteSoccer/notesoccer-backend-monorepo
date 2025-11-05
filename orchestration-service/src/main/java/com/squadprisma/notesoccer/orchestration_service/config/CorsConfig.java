c7b356090e869b979c7bf1cdea7632dc384e1c5f
Leonardo A. C. Ferraz <leoalvo89@gmail.com>
2025-11-05 09:50:38 -0300

    chore: adicionar configuração global de CORS no orchestration-service

---
+++ orchestration-service/src/main/java/com/squadprisma/notesoccer/orchestration_service/config/CorsConfig.java
@@ -0,0 +1,29 @@
+package com.squadprisma.notesoccer.orchestration_service.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer(){
+        return new WebMvcConfigurer(){
+            @Override
+            public void addCorsMappings(CorsRegistry registry){
+                registry.addMapping("/api/**")
+                        .allowedOrigins(
+                                "http://localhost:4200",
+                                "https://notesoccer.netlify.app"
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .exposedHeaders("Location")
+                        .allowCredentials(true)
+                        .maxAge(3600);
+            }
+        };
+    }
+}
