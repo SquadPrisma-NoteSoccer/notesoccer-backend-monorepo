833dfe2e58e2ba09383c3c6ca69b5ef9c33c1095
Leonardo Cuba Ferraz <97860586+leoacferraz@users.noreply.github.com>
2025-11-13 18:57:20 -0300

    Configs para usar o h2 para os testes ci/cd (#44)

--- league-service/src/test/java/com/squadprisma/notesoccer/league_service/LeagueServiceApplicationTests.java
+++ league-service/src/test/java/com/squadprisma/notesoccer/league_service/LeagueServiceApplicationTests.java
@@ -3,9 +3,11 @@ package com.squadprisma.notesoccer.league_service;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @Disabled("Context full não necessário por enquanto neste módulo")
 @SpringBootTest
+@ActiveProfiles("test")
 class LeagueServiceApplicationTests {
 
 	@Test
